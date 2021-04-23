7bdd765bb1305c63cca2e1d1d457b1770d884b02
Inviolacy Edulcorate <inviolacy_edulcorate@aleeas.com>
2021-04-23 18:12:10 +0200

    Implements AOC 2015 Day 01 Part 2

---
+++ PascalFiles/AOC/2015/01-2.pas
@@ -0,0 +1,34 @@
+program Main;
+	var text : string;
+		i, inputLength, floor : integer;
+		keepLoopGoing : boolean;
+		ex01, ex02, ex03, ex04, ex05,ex06, ex07, ex08, ex09 : string;
+		
+begin { Main }
+	ex01 := ')'; {1}
+	ex02 := '()())'; {5}
+
+	text := ReadFile('Input\01.txt');
+	{text := ex02;}
+	inputLength := Length(text);
+	keepLoopGoing := true;
+	i := 0;
+	floor := 0;
+	
+	while keepLoopGoing do
+	begin
+		if (i - inputLength = 0) then keepLoopGoing := false
+		else
+		begin
+			if (text[i] = '(') then floor := floor + 1;
+			if (text[i] = ')') then floor := floor - 1;
+
+			if (floor = -1) then keepLoopGoing := false;
+			
+			{Can leave this here, +1 is needed anyway because of 0 indexing.}
+			i := i + 1;
+		end;
+	end;
+	
+	writeln(i);
+end.  { Main }
